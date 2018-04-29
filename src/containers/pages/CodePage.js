391966bf33ab216efb67ccb53c8cc5a46544f2b3
00tanveer <tanveer_fh09@yahoo.com>
2018-04-29 13:00:52 +0600

    made some changes in Editor

--- src/containers/pages/CodePage.js
+++ src/containers/pages/CodePage.js
@@ -2,47 +2,8 @@ import React from 'react';
 import ReactQuill from 'react-quill';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
-
-// const CustomToolbar = () => (
-//     <div id="toolbar">
-//       <select className="ql-header" defaultValue={""} onChange={e => e.persist()}>
-//         <option value="1" />
-//         <option value="2" />
-//         <option selected />
-//       </select>
-//       <button className="ql-bold" />
-//       <button className="ql-italic" />
-//       <select className="ql-color">
-//         <option value="red" />
-//         <option value="green" />
-//         <option value="blue" />
-//         <option value="orange" />
-//         <option value="violet" />
-//         <option value="#d0d1d2" />
-//         <option selected />
-//       </select>
-//       <button className="ql-blockquote" />
-//       <button className="ql-image" />
-//       <button className="ql-strike" />
-//     </div>
-//   );
-
 class CodePage extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = { 
-    //         text: ''
-    //         //CustomToolbar: StyledCustomToolbar
-    // } // You can also pass a Quill Delta here
-    //   this.handleChange = this.handleChange.bind(this)
-    // }
-  
-    // handleChange(value) {
-    //   this.setState({ text: value })
-    // }
     
-    
-
     render() { 
       const Button = styled.button`
         display: inline-block;
