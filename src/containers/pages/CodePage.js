53ea9af0384143aed1b918b7efab3903933c9546
00tanveer <tanveer_fh09@yahoo.com>
2018-09-11 10:33:09 +0600

    cleaned up CodePage

--- src/containers/pages/CodePage.js
+++ src/containers/pages/CodePage.js
@@ -1,39 +1,14 @@
-import React from 'react';
-import Button from '../../components/ui/Button';
-import axios from 'axios';
+import React from "react";
+import Button from "../../components/ui/Button";
+import axios from "axios";
 class CodePage extends React.Component {
-    state = {
-      blogs: []
-    }
-  
-  componentDidMount(){
-    axios.get('/blogs/blogs')
-      .then(res => {
-        //console.log(res.data.data.docs[0]);
-        console.log(res.data.data);
-        const blogs = res.data.data.docs;
-        this.setState({blogs});
-        console.log(this.state.blogs);
-      })
-      .catch(function(err){
-        console.log(err);
-      })
-  }
-  render() { 
-    const {isAuthenticated}  = this.props.auth;
-      return (
-        <div>
-          {
-            isAuthenticated() && (
-              < Button link label = 'Post a blog'
-              to = '/post' / >
-            )
-          }
-          <ul>
-            {this.state.blogs.map(blog => <li>{blog.title}</li>)}
-          </ul>
-        </div>
-    );
+  state = {
+    blogs: []
+  };
+
+  componentDidMount() {}
+  render() {
+    return <div style={{ color: "white", fontSize: "6em" }}>HELLO</div>;
   }
 }
 
